ec2dc0cae0f97153afe806636fbef03dc6bb5163
keitaromiura-link <k.miura@reservelink.co.jp>
2018-07-23 15:09:30 +0900

    連続した3つの数字以内になったらコンセンサスが取れたとして平均値をだす#1
    試しにコミット

--- store/index.js
+++ store/index.js
@@ -57,14 +57,63 @@ class Room extends Immutable.Record({
   }
 
   result () {
-    const first = this.get('players').first();
-    const p = first.point;
+    //const first = this.get('players').first();
+    //const p = first.point;
 
-    if (!(_.isNumber(p) && p >= 0 && p <= 100)) return '';
+    //if (!(_.isNumber(p) && p >= 0 && p <= 100)) return '';
 
     // 異なるポイントの要素があったら非合意
-    const difference = this.get('players').find(player => player.point !== p);
-
+    // @todo 異なるポイントの要素が連続する3つ以内じゃなかったら。
+    
+    // const difference = this.get('players').find(player => player.point !== p);
+    const max = this.get('players').maxBy(player => player.point);
+    const min = this.get('players').minBy(player => player.point);
+    const difference = true;
+    if (max === min) {
+        difference = false;
+    }else if (max === 89 && min === 55){
+        difference = false;
+    }else if (max === 89 && min === 34){
+        difference = false;
+    }else if (max === 55 && min === 34){
+        difference = false;
+    }else if (max === 55 && min === 21){
+        difference = false;
+    }else if (max === 34 && min === 21){
+        difference = false;
+    }else if (max === 34 && min === 13){
+        difference = false;
+    }else if (max === 21 && min === 13){
+        difference = false;
+    }else if (max === 21 && min === 8){
+        difference = false;
+    }else if (max === 13 && min === 8){
+        difference = false;
+    }else if (max === 13 && min === 5){
+        difference = false;
+    }else if (max === 8 && min === 5){
+        difference = false;
+    }else if (max === 8 && min === 3){
+        difference = false;
+    }else if (max === 5 && min === 3){
+        difference = false;
+    }else if (max === 5 && min === 2){
+        difference = false;
+    }else if (max === 3 && min === 2){
+        difference = false;
+    }else if (max === 3 && min === 1){
+        difference = false;
+    }else if (max === 2 && min === 1){
+        difference = false;
+    }else if (max === 2 && min === 0.5){
+        difference = false;
+    }else if (max === 1 && min === 0.5){
+        difference = false;
+    }else if (max === 1 && min === 0){
+        difference = false;
+    }else if (max === 0.5 && min === 0){
+        difference = false;
+    }
     return difference ? '' : 'consensus';
   }
 }
